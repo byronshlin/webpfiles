cfceb153956a851e26a6e885367de48ccc277417
Byron Lin <byronlin@catchplay.com>
2017-12-25 00:33:11 +0800

    files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'webpfiles',
+    packages = ['webpfiles'],
+    scripts = ['runner'],
+    version = '1.0',
+    description = 'transfer png to webp on Android App source code',
+    author = 'byronlin',
+    author_email = 'byronlin0614@gmail.com',
+    url = 'https://github.com/byronshlin/webpfiles',
+    download_url = 'https://github.com/byronshlin/webpfiles',
+    keywords = ['webp'],
+    classifiers = [],
+)
